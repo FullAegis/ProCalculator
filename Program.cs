b3763b954b2635b70356e9d3aa7d81ad5e1396a7
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-02-26 16:48:54 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 ﻿using System;
+using ProCalculator;
 
 class Program {
-
-  
-  private Stack<Operators> operators;
-  private Stack<double> numbers;
-  
   private static string Prompt(string prompt) {
     Console.Write(prompt);
     return Console.ReadLine() ?? string.Empty;
   }
-  
-  public static void Main (string[] args) {
+
+  public static void Main(string[] args) {
     var input = Prompt("Please enter an equation: ");
-    
-    
+    var calc = new Calculator(input);
+    Console.WriteLine($"Result of {calc.Equation}: {calc.Evaluate()}");
   }
 }
 
