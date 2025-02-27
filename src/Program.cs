edd1d8eae8b6da7e1f79f79047c3eb01a98336d3
Anthony Capobianco <a.capobianco@stag.technofuturtic.education>
2025-02-27 09:49:27 +0100

    Folder structure

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ProCalculator;
+
+class Program {
+  private static string Prompt(string prompt) {
+    Console.Write(prompt);
+    return Console.ReadLine() ?? string.Empty;
+  }
+
+  public static void Main(string[] args) {
+    var input = Prompt("Please enter an equation: ");
+    var calc = new Calculator(input);
+    Console.WriteLine($"Result of {calc.Equation}: {calc.Evaluate()}");
+  }
+}
+
